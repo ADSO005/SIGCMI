4f8c3210aedbaba186c265b516da1f3f4b9557c9
Juan Montoya <montoyajuanes11@gmail.com>
2026-04-17 09:38:02 -0500

    I'm implementing an ADMIN statistics section and a bit of accessibility.

--- frontend/javascript/dashboardADMIN.js
+++ frontend/javascript/dashboardADMIN.js
@@ -1,10 +1,11 @@
 const btn = document.getElementById("toggleBtn");
 const sidebar = document.getElementById("sidebar");
+let main = document.querySelector(".main");
 
 btn.onclick = () => {
   sidebar.classList.toggle("collapsed");
 
   btn.style.marginLeft =
-    sidebar.classList.contains("collapsed") ? "10px" : "5%";
-
+    sidebar.classList.contains("collapsed") ? "9px" : "5%";
+    main.classList.toggle("collapsed");
 };
